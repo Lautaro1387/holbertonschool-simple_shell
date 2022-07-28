c0ac95efdc22826aa9ba2982ebf7f7a3c8307a4e
Lautaro1387 <5134@holbertonschool.com>
2022-07-28 11:55:30 -0700

    Nashe

--- lauta/getenv.c
+++ lauta/getenv.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
-char *_getenv(const char *name)
+char *_getenv(const char *name, char **env)
 {
+	int i = 0;
+	char *dup;
 
+	while (env[i] && _strcmp(name, dup))
+	{
+		printf("%s\n", env[i++]);
+	}
+	if (env[i])
+	{
+
+	}
+	return (0);
 }
