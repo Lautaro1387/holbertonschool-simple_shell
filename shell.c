2f089726256b6af650fe97dc4d7a9d503c7e3afa
Lautaro1387 <5134@holbertonschool.com>
2022-08-01 15:34:58 -0700

    Finished?

--- shell.c
+++ shell.c
@@ -6,21 +6,26 @@
 *
 */
 
-int main(__attribute__((unused))int ac, char **av)
+int main(__attribute__((unused)) int ac,  __attribute__((unused)) char **av, char **env)
 {
-	int status = 0;
-	char *args[] = {"", NULL};
-	size_t len = 0;
 	char *buff = NULL;
+	int status = 0;
+	char *args[1024];
+	size_t str, len = 0;
+	char *token;
 
-	(void) av;
 	while (1)
 	{
 		if (getline(&buff, &len, stdin) == -1)
 			break;
-		token_checker(buff, args, "\t\n");
-		args[0] = check_space(strdup(buff));
-		if (!args[0] || !args[0][0])
+		token = strtok(buff, "\t\n\r");
+		for (str = 0; str < 1024 && token != NULL; str++)
+		{
+			args[str] = token;
+			token = strtok(NULL, "\t\n\r");
+		}
+		args[str] = NULL;
+		if (!args[0])
 		{
 			free(args[0]);
 			free(buff);
@@ -28,17 +33,14 @@ int main(__attribute__((unused))int ac, char **av)
 		}
 		if (fork() == 0)
 		{
-			if (execve(args[0], args, NULL) == -1)
+			if (execve(args[0], args, env) == -1)
 			{
 				perror("Error");
 				return (0);
 			}
 		}
 		else
-		{
 			wait(&status);
-		}
-		free(args[0]);
 	}
 	free(buff);
 	return (0);
